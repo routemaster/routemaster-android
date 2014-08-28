6ace80e77818cfb2b11cc8fb6b0b771f14622065
Benjamin Woodruff <benjaminwoodruff@ufl.edu>
2014-08-27 22:00:18 -0400

    Add a "Hello, world" app using gradle
    http://i.imgur.com/53pccbf.jpg

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-routemaster-android
-===================
+RouteMaster (Android App)
+=========================
 
-The RouteMaster Android App
+Walking is boring. Let's turn it into an online multiplayer competitive game!
+
+- Map your walking, running, biking, or even driving paths with GPS on your
+  phone.
+  - View past routes, and the routes of your friends
+  - Earn points for walking efficient routes
+  - Earn points for exploring more area than your friends
+
+Building
+--------
+
+```sh
+$ ./gradlew
+```
+
+or, on Windows,
+
+```sh
+$ gradlew.bat
+```
---
+++ src/main/java/org/routemaster/MainActivity.java
@@ -0,0 +1,49 @@
+package org.routemaster;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
+import android.os.Bundle;
+import com.google.common.collect.ImmutableList;
+
+public class MainActivity extends Activity {
+
+    final TabListener dummyTabListener = new TabListener() {
+        @Override
+        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        }
+
+        @Override
+        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+        }
+
+        @Override
+        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        Tab recordTab = actionBar.newTab()
+                                 .setText("Record")
+                                 .setTabListener(dummyTabListener);
+        Tab historyTab = actionBar.newTab()
+                                  .setText("History")
+                                  .setTabListener(dummyTabListener);
+        Tab friendsTab = actionBar.newTab()
+                                  .setText("Friends")
+                                  .setTabListener(dummyTabListener);
+        actionBar.addTab(recordTab);
+        actionBar.addTab(historyTab);
+        actionBar.addTab(friendsTab);
+        setContentView(R.layout.activity_main);
+
+        // Something to test our dependencies
+        ImmutableList.of(1, 2, 3);
+    }
+}
