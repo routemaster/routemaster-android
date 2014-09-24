a4eee57257a2e8cd1c14a315c2ddc5f26426113c
Benjamin Woodruff <benjaminwoodruff@ufl.edu>
2014-09-23 22:23:54 -0400

    Draw a line on the map as the user walks
    This is a first-iteration at requesting location updates from
    FusedLocationProviderApi. It draws on the map, and makes some updates to
    the surrounding codebase.
    
    -   Google Play Services 5.0.+ is now required.
    -   We now use Java 7 instead of the default, 6. Java 8 isn't supported
        on Android.
    -   The main activity is now reponsible for connecting to Google Play
        Services.
    -   We now extend MapFragment, allowing us to properly determine when
        getMap() has likely been constructed, and allowing us to encapuslate
        the display of routes.
    
    A number of issues currently exist.
    
    -   Error handling is very poor.
    -   Tracking stops if the activity is destroyed, eg, when the screen is
        turned off for an extended period of time.
    -   Low-accuracy readings may block high-accuracy readings from being
        recorded if they're within 10 meters of the high-accuracy reading.

---
+++ src/org/lumeh/routemaster/record/LocationGoogleApiClientCallbacks.java
@@ -0,0 +1,49 @@
+package org.lumeh.routemaster.record;
+
+import android.os.Bundle;
+import android.util.Log;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+/**
+ * Upon connecting to Google Play Services, submits a request for location
+ * updates.
+ * <p>
+ * Uses some tricks described on:
+ * http://www.rahuljiresal.com/2014/02/user-location-on-android/
+ */
+public class LocationGoogleApiClientCallbacks
+                     implements GoogleApiClient.ConnectionCallbacks,
+                                GoogleApiClient.OnConnectionFailedListener {
+    private static final String TAG = "RouteMaster";
+    private final GoogleApiClient apiClient;
+    private final LocationRequest request;
+    private final LocationListener listener;
+
+    public LocationGoogleApiClientCallbacks(GoogleApiClient apiClient,
+                                            LocationRequest request,
+                                            LocationListener listener) {
+        this.apiClient = apiClient;
+        this.request = request;
+        this.listener = listener;
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+            apiClient, request, listener
+        );
+        Log.d(TAG, "requested location updates");
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) { }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // TODO: fall back to LocationManager
+    }
+}
--- src/org/lumeh/routemaster/record/RecordFragment.java
+++ src/org/lumeh/routemaster/record/RecordFragment.java
@@ -1,30 +1,74 @@
 package org.lumeh.routemaster.record;
 
 import android.app.Fragment;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMapOptions;
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+import java.util.ArrayList;
 import org.lumeh.routemaster.R;
 
-public class RecordFragment extends Fragment {
+public class RecordFragment extends Fragment implements LocationListener {
+    private static final String TAG = "RouteMaster";
+
     private static final String TAG_MAP_FRAGMENT = "mapFragment";
+    private static final String TAG_ROUTE_LOCATIONS = "routeLocations";
+    private static final String TAG_ROUTE_POINTS = "routePoints";
 
-    private MapFragment mapFragment;
-    private boolean mapIsConfigured;
+    private static final LocationRequest LOCATION_REQUEST =
+        LocationRequest.create()
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+            .setSmallestDisplacement(10.0f) // meters, saves power
+            .setInterval(1000); // ms, actual interval may differ
 
-    public void onCreate(Bundle state) {
-        super.onCreate(state);
-    }
+    /**
+     * If a location has a worse accuracy than this value, it's completely
+     * discarded, because it's of no use to us. This is useful when the app is
+     * first starting, so it doesn't use a bad value before the gps/wifi is
+     * ready.
+     */
+    private static final float MIN_ACCURACY = 20.0f; // meters
+
+    /**
+     * If the distance between the current and last locations are larger than
+     * this our recording is bad, and should be discarded. (TODO)
+     * <p>
+     * Santity check: 60 miles per hour is about 26.82 meters per second
+     */
+    private static final float MAX_DISTANCE = 40.0f; // meters
+
+    private GoogleApiClient googleApiClient;
+    private TrackingMapFragment mapFragment;
+    private LocationGoogleApiClientCallbacks locationCallbacks;
+
+    /**
+     * Store the raw Location objects received from onLocationChanged, allowing
+     * further processing later.
+     */
+    private ArrayList<Location> routeLocations = new ArrayList<>();
+
+    /**
+     * Store visited locations as LatLng, the data format Polyline requires,
+     * avoiding converting the routeLocations structure on every update.
+     */
+    private ArrayList<LatLng> routePoints = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle state) {
         super.onCreateView(inflater, container, state);
         return inflater.inflate(R.layout.record, container, false);
+    }
+
+    public void setGoogleApiClient(GoogleApiClient googleApiClient) {
+        this.googleApiClient = googleApiClient;
     }
 
     /**
@@ -36,55 +80,79 @@ public class RecordFragment extends Fragment {
     public void onActivityCreated(Bundle state) {
         super.onActivityCreated(state);
 
-        // ignore if we're called on re-attach
-        if(getMapFragment() != null) {
-            return;
+        // find mapFragment in the view hierarchy
+        if(mapFragment == null) {
+            mapFragment = (TrackingMapFragment) getChildFragmentManager()
+                .findFragmentByTag(TAG_MAP_FRAGMENT);
         }
 
-        if(state == null) {
-            MapFragment mapFragment = MapFragment.newInstance(
-                new GoogleMapOptions().zoomControlsEnabled(false)
-            );
+        // mapFragment wasn't in the view hierarchy, so we need to create it
+        if(mapFragment == null) {
+            mapFragment = new TrackingMapFragment();
             getChildFragmentManager().beginTransaction()
                 .add(R.id.record, mapFragment, TAG_MAP_FRAGMENT)
                 .commit();
         }
-        mapIsConfigured = false;
-    }
 
-    public MapFragment getMapFragment() {
-        return (MapFragment) getChildFragmentManager()
-            .findFragmentByTag(TAG_MAP_FRAGMENT);
-    }
-
-    public GoogleMap getMap() {
-        return getMapFragment().getMap();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // we have to wait until start, because the MapFragment creates the
-        // underlying map in its onCreateView method.
-        if(!mapIsConfigured) {
-            configureMap();
-            mapIsConfigured = true;
+        // restore from state
+        if(state != null) {
+            routeLocations = state.getParcelableArrayList(TAG_ROUTE_LOCATIONS);
+            routePoints = state.getParcelableArrayList(TAG_ROUTE_POINTS);
+            getMapFragment().setRoutePoints(routePoints);
         }
     }
 
-    /**
-     * Gets called *after* the map fragment's underlying GoogleMap instance is
-     * contructed. This happens when the fragment transaction is executed.
-     * {@code map} should not be null at this point.
-     * <p>
-     * We can use {@link GoogleMapOptions} for some things, but it's incomplete.
-     */
-    private void configureMap() {
-        getMap().setMyLocationEnabled(true);
-        getMap().getUiSettings().setMyLocationButtonEnabled(true);
-        // TODO: Figure out some way of calling setRetainInstance(true). This
-        // would allow us to handle rotation without any flashing of the map.
-        // Android doesn't support retaining nested fragments.
+    public void onStart() {
+        super.onStart();
+        // Asynchronously request location updates upon connecting to Google
+        // Play Services
+        if(locationCallbacks == null) {
+            locationCallbacks = new LocationGoogleApiClientCallbacks(
+                googleApiClient, LOCATION_REQUEST, this
+            );
+            googleApiClient.registerConnectionCallbacks(locationCallbacks);
+            googleApiClient.registerConnectionFailedListener(locationCallbacks);
+        }
+    }
+
+    public TrackingMapFragment getMapFragment() {
+        return mapFragment;
+    }
+
+    @Override
+    public void onLocationChanged(Location loc) {
+        Log.d(TAG, "location update");
+
+        // FIXME: When there's a low-accuracy reading followed by a
+        // high-accuracy reading within 10m, the location never actually gets
+        // read, because the LocationApi doesn't fill us on on better accuracy
+        // readings. We need to make *another* LocationRequest when this happens
+        // for a one-off reading.
+        //
+        // Another person with the same problem:
+        // http://stackoverflow.com/q/22365188/130598
+        if(loc.getAccuracy() > MIN_ACCURACY) {
+            Log.d(TAG, "ignoring inaccurate location");
+            return;
+        }
+
+        if(routeLocations.size() > 0) {
+            Location prevLoc = routeLocations.get(routeLocations.size() - 1);
+            if(prevLoc.distanceTo(loc) > MAX_DISTANCE) {
+                // TODO: actually discard
+                Log.w(TAG, "route has a large jump, should be discarded");
+            }
+        }
+
+        routeLocations.add(loc);
+        routePoints.add(new LatLng(loc.getLatitude(), loc.getLongitude()));
+        getMapFragment().setRoutePoints(routePoints);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle state) {
+        state.putParcelableArrayList(TAG_ROUTE_LOCATIONS, routeLocations);
+        state.putParcelableArrayList(TAG_ROUTE_POINTS, routePoints);
     }
 
     /**
