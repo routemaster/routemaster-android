5a88f459773c3ec23858cc781f1eb220e0614229
Benjamin Woodruff <github@benjam.info>
2014-10-21 00:07:22 -0400

    Switch to a ViewPager for tabs
    In Android 5.0 and the associated support libraries, ActionBar
    navigation is deprecated. This keeps us from styling the elements
    properly.
    
    We gain the ability to swipe between views (except when swiping over a
    map).
    
    However, we lose the nice compact view created on tablets and on
    landscape mode where the tabs were merged into the title bar. I've got
    a few ideas on how to mitigate this.

--- src/org/lumeh/routemaster/MainActivity.java
+++ src/org/lumeh/routemaster/MainActivity.java
@@ -4,15 +4,13 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.ActionBar.Tab;
-import android.support.v7.app.ActionBar.TabListener;
-import android.support.v7.app.ActionBar;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.widget.Toast;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.location.LocationServices;
+import com.astuetz.PagerSlidingTabStrip;
 import com.google.common.collect.ImmutableList;
+import org.lumeh.routemaster.FragmentListPagerAdapter.TabEntry;
 import org.lumeh.routemaster.history.HistoryFragment;
 import org.lumeh.routemaster.record.RecordFragment;
 
@@ -27,6 +25,8 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(state);
 
         setContentView(R.layout.main);
+        getSupportFragmentManager().executePendingTransactions();
+
         if(state == null) {
             onInitialCreate();
         } else {
@@ -34,12 +34,6 @@ public class MainActivity extends ActionBarActivity {
         }
 
         addTabs();
-
-        // select the previous tab, if there was one
-        if(state != null) {
-            int tabId = state.getInt(STATE_SELECTED_TAB_ID);
-            getSupportActionBar().setSelectedNavigationItem(tabId);
-        }
     }
 
     /**
@@ -48,15 +42,6 @@ public class MainActivity extends ActionBarActivity {
      */
     protected void onInitialCreate() {
         Log.i(TAG, "Welcome to RouteMaster!");
-
-        // create fragments
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment recordFragment = new RecordFragment();
-        Fragment historyFragment = new HistoryFragment();
-        fm.beginTransaction()
-            .add(R.id.main, recordFragment, TAG_RECORD_FRAGMENT)
-            .add(R.id.main, historyFragment, TAG_HISTORY_FRAGMENT)
-            .commit();
 
         // TODO: add better error handling
         LocationManager manager =
@@ -70,48 +55,21 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-    public RecordFragment getRecordFragment() {
-        return (RecordFragment) getSupportFragmentManager()
-            .findFragmentByTag(TAG_RECORD_FRAGMENT);
-    }
-
-    public HistoryFragment getHistoryFragment() {
-        return (HistoryFragment) getSupportFragmentManager()
-            .findFragmentByTag(TAG_HISTORY_FRAGMENT);
-    }
-
     /**
      * Add a record and history tab, with their correct labels.
      */
     protected void addTabs() {
-        getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        FragmentListPagerAdapter adapter = new FragmentListPagerAdapter(
+            getSupportFragmentManager(),
+            new TabEntry("Record", RecordFragment.class),
+            new TabEntry("History", HistoryFragment.class)
+        );
 
-        // fragment transactions are executed async in parallel with the main
-        // thread. getRecordFragment() and getHistoryFragment() pull the
-        // fragments from the view hierarchy.
-        getSupportFragmentManager().executePendingTransactions();
+        ViewPager pager = (ViewPager) findViewById(R.id.main);
+        pager.setAdapter(adapter);
 
-        addTab("Record", getRecordFragment()).select();
-        addTab("History", getHistoryFragment());
-    }
-
-    protected Tab addTab(String label, Fragment fragment) {
-        getSupportFragmentManager()
-            .beginTransaction()
-            .detach(fragment)
-            .commit();
-        Tab tab = getSupportActionBar().newTab()
-            .setText(label)
-            .setTabListener(new FragmentTabListener(fragment));
-        getSupportActionBar().addTab(tab);
-        return tab;
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle state) {
-        super.onSaveInstanceState(state);
-        FragmentManager fm = getSupportFragmentManager();
-        int tabId = getSupportActionBar().getSelectedNavigationIndex();
-        state.putInt(STATE_SELECTED_TAB_ID, tabId);
+        PagerSlidingTabStrip tabs =
+            (PagerSlidingTabStrip) findViewById(R.id.main_tabs);
+        tabs.setViewPager(pager);
     }
 }
